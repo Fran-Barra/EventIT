892e7147c92460d6754ebbdddac93348490d840b
carlixxxx <88391740+carlixxxx@users.noreply.github.com>
2021-10-17 14:44:45 -0300

    Create ABM.py
    Dar alta y agregar admin, necesitan de la clase de Crear_Usuario, por eso aun no están hechos.

---
+++ EventIT/UsersLib.py/ABM.py
@@ -0,0 +1,11 @@
+class ABM:
+  
+  @classmethod
+	def DarBaja(self):
+		return f"El usuario {usuario} se dio de baja satisfactoriamente."
+  @classmethod
+	def Modificar(self): 
+		return f"El usuario {usuario} fue modificado, satisfactoriamente."
+  
+
+
