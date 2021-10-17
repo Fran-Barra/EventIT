30b22263c21403a9055f4778dc72bcb778afd8d5
Tomassfreile <92683588+Tomassfreile@users.noreply.github.com>
2021-10-17 18:55:28 -0300

    Update EventManager.py

--- EventIT/EventLib/EventManager.py
+++ EventIT/EventLib/EventManager.py
@@ -7,3 +7,12 @@ class EventManger:
     def altaEvento(self, TipoDeEvento, User):
         if User.isinstance(Administrator):
             return f"El administrador {User} dio de alta {TipoDeEvento}"
+    def ReportEvent(self, invitados, caracteristicas):
+        pass
+
+    def AsistirEvento(self,Evento, invitados):
+        pass
+    
+    def DesinscribirseEvento(self,Evento):
+        pass
+
