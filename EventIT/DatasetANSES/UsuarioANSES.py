1c40129509c0756146af1f13ae219166004fea89
lucaslovaglio <92699011+lucaslovaglio@users.noreply.github.com>
2021-10-17 23:56:14 -0300

    Update UsuarioANSES.py

--- EventIT/DatasetANSES/UsuarioANSES.py
+++ EventIT/DatasetANSES/UsuarioANSES.py
@@ -12,21 +12,3 @@ class UsuarioANSES:
 
     def getCuil(self):
         return self.__cuil
-
-
-# usuarios_ANSES = open('usuarios_ANSES.txt', 'r')
-# for linea in usuarios_ANSES.readlines():
-#     usuario = linea
-#     firstBar = usuario.index("/")
-#     secondBar = usuario.index("/", firstBar + 1)
-#     thirdBar = usuario.index("/", secondBar + 1)
-#     lenOfName = usuario.index("/")
-#     lenOfTelCell = usuario.index("/", firstBar + 1) - firstBar - 1
-#     lenOfCuil = usuario.index("/", secondBar + 1) - secondBar - 1
-#     nombre = usuario[: firstBar]
-#     telCell = usuario[firstBar + 1 : secondBar]
-#     cuil = usuario[secondBar + 1 : thirdBar]
-#
-#
-# usuarios_ANSES.close()
-
