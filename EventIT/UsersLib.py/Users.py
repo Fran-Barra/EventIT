dddb9ae092feaa65ecfeccd25a27e8d57d3e6353
lucaslovaglio <92699011+lucaslovaglio@users.noreply.github.com>
2021-10-20 23:26:24 -0300

    Update Users.py

--- EventIT/UsersLib.py/Users.py
+++ EventIT/UsersLib.py/Users.py
@@ -1,3 +1,7 @@
-class Usuario:
+from abc import ABC, abstractmethod
+
+
+class Usuario(ABC):
+    @abstractmethod #no estoy seguro si el __init__ puede ser @abstractmethod
     def __init__(self, Name):
         self.__Name = Name
