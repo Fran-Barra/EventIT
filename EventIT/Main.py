2845742bc23085b3a6e491b7721648731099abcd
Franco B <73652543+GrlGhost@users.noreply.github.com>
2021-10-22 09:02:44 -0300

    Update Main.py
    Agregue una instancia para el data anses

--- EventIT/Main.py
+++ EventIT/Main.py
@@ -1,5 +1,8 @@
-from UsersLib.py.RegDeUsuarios import RegDeUsuarios
 from EventLib.RegDeEventosClass import RegDeEventos
+from UsersLib.RegDeUsuarios import RegDeUsuarios
+from DatasetANSES.DatasetANSES import DatasetANSES
 
 regdeusuarios = RegDeUsuarios()
+regANSES = DatasetANSES()
 regdeeventos = RegDeEventos()
+
