d7d2d5b22cee002b3afa3374a8ae0adadc82e13e
Tomassfreile <92683588+Tomassfreile@users.noreply.github.com>
2021-10-28 19:20:24 -0300

    Create FrienshipSist.py

---
+++ EventIT/FrienshipSist.py
@@ -0,0 +1,30 @@
+class Frienship_Sistem:
+    def __init__(self):
+        self.personas_rechazadas = []
+
+    def get_personas_rechazadas(self):
+        return self.personas_rechazadas
+
+    def EnviarSolicitud(self, CiudadanoSolicitante, CiudadanoDestinatario):
+        if CiudadanoSolicitante not in CiudadanoDestinatario.Get_ListaDeSolicitudes():
+            a = CiudadanoDestinatario.Mod_ListaDeSolicitudes()
+            a.append(CiudadanoSolicitante)
+        else:
+            return "Ya le enviaste una solicitud a este ciudadano"
+
+    def AceptarSolicitud(self, CiudadanoSolicitante, CiudadanoDestinatario):
+        if CiudadanoSolicitante in CiudadanoDestinatario.Get_ListaDeSolicitudes():
+            a = CiudadanoDestinatario.Mod_ContactosDeInteres()
+            a.append(CiudadanoSolicitante)
+            b = CiudadanoDestinatario.Mod_ListaDeSolicitudes()
+            b.remove(CiudadanoSolicitante)
+        else:
+            return "No tienes solicitudes de este ciudadano"
+
+    def RechazarSolicitud(self, CiudadanoSolicitante, CiudadanoDestinatario):
+        if CiudadanoSolicitante in CiudadanoDestinatario.Get_ListaDeSolicitudes():
+            self.personas_rechazadas.append(CiudadanoSolicitante)
+            a = CiudadanoDestinatario.Mod_ListaDeSolicitudes()
+            a.remove(CiudadanoSolicitante)
+        else:
+            return "No tienes solicitudes de este usuario"
