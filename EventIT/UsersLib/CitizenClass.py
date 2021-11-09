069af039479a78b0d12a7811f6516c52e9791e2c
Tomassfreile <92683588+Tomassfreile@users.noreply.github.com>
2021-11-09 20:35:28 -0300

    Update CitizenClass.py

--- EventIT/UsersLib/CitizenClass.py
+++ EventIT/UsersLib/CitizenClass.py
@@ -22,6 +22,9 @@ class Ciudadano(Usuario):
 
     def Get_ListaDeSolicitudes(self):
         return self.__ListaDeSolicitudes.copy()
+    
+    def Get_ListaDeRechazos(self):
+        return self.__ListaDeRechazos.copy()
 
     def Mod_Name(self, name: str):
         #Solo la puede llamar el AMB
