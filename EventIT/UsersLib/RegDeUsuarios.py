d1d146e958fd5b5e761f2ee5c5e18803feabd4d8
luckylovaglio <luckylovaglio@gmail.com>
2021-11-13 16:16:16 -0300

    update prueba.py

--- EventIT/UsersLib/RegDeUsuarios.py
+++ EventIT/UsersLib/RegDeUsuarios.py
@@ -20,7 +20,7 @@ class RegDeUsuarios:
 
     def searchCitizen(self, telCell: int = None, cuil: int = None, name: str = None):
         if cuil == None and telCell == None and name == None:
-            alert = tk.messagebox.showwarning(title="Falta de argumentos", text="Para buscar un ciudadano es necesario que ")
+            alert = tk.messagebox.showwarning(title="Falta de argumentos", text="Para buscar un ciudadano es necesario que introduzca al menos un argumento")
         for ciudadano in self.__Ciudadanos:
             cuil = ciudadano.Get_Cuil() if cuil == None else cuil
             telCell = ciudadano.Get_Telefono() if telCell == None else telCell
