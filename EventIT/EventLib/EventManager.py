9b1762418803dbfd9b6df2217e115a3190607217
Tomassfreile <92683588+Tomassfreile@users.noreply.github.com>
2021-11-14 22:14:49 -0300

    Update EventManager.py

--- EventIT/EventLib/EventManager.py
+++ EventIT/EventLib/EventManager.py
@@ -1,12 +1,12 @@
-from AdminClass import Administrator
+
 
 class EventManger:
     def __init__(self):
         self.TiposDeEvento = []
 
     def altaEvento(self, TipoDeEvento, User):
-        if User.isinstance(Administrator):
-            return f"El administrador {User} dio de alta {TipoDeEvento}"
+        pass
+    
     def ReportEvent(self, invitados, caracteristicas):
         pass
 
