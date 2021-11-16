6b47ec149b0b040ba9d2cb65c8aa9250458b201f
carlixxxx <88391740+carlixxxx@users.noreply.github.com>
2021-11-16 14:47:16 -0300

    Update EventManager.py

--- EventIT/EventLib/EventManager.py
+++ EventIT/EventLib/EventManager.py
@@ -1,18 +1,33 @@
-
+from AdminClass import Administrator
+from RegDeEventosClass import RegDeEventos
+from EventoClass import Evento
 
 class EventManger:
-    def __init__(self):
-        self.TiposDeEvento = []
 
-    def altaEvento(self, TipoDeEvento, User):
-        pass
-    
-    def ReportEvent(self, invitados, caracteristicas):
-        pass
+    def __init__(self, RegDeEvento):
+        self.__RegDeEventos = RegDeEvento
+        self.__TipoDeEventos = []
 
-    def AsistirEvento(self,Evento, invitados):
-        pass
-    
-    def DesinscribirseEvento(self,Evento):
-        pass
+    def alta_tiposDeEvento(self, TipoDeEvento, User):
+        # El admin da de alta tipos de eventos, para que a la hora de crear eventos solo sean de los tipos de eventos aprobados.
+        if User.isinstance(Administrator) and TipoDeEvento not in self.__TipoDeEventos:
+            self.__TipoDeEventos.append(TipoDeEvento)
+
+    def ver_tiposDeEvento (self):
+        return self.__TipoDeEventos.copy()
+
+    def report_evento(self, tipo, ubicacion):
+        # Se guardan nuevos eventos en lista de eventos del RegDeEventos
+        if tipo in self.__TipoDeEventos:
+            self.__RegDeEventos.Set_Events().append(Evento(tipo, ubicacion))
+
+    def asistir_evento(self, evento, usuario):
+        # Si el evento existe en la lista de eventos, se agrega a la lista de invitados el nuevo invitado.
+        if evento in self.__RegDeEventos.View_Events():
+            evento.Set_Attendance().append(usuario)
+
+    def desinscribirse_evento(self, evento, usuario):
+        # Si el evento esta en la lista y el usuario figura como invitado, se lo elimina de la lista de invitados.
+        if evento in self.__RegDeEventos.View_Events() and usuario in evento.Set_Attendance():
+            evento.Set_Attendance().remove(usuario)
 
