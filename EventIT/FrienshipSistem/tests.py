1219fbc7def20bf860d0ae854ec7f3e52d99e1ae
Tomassfreile <92683588+Tomassfreile@users.noreply.github.com>
2021-11-17 18:44:20 -0300

    Update tests.py

--- EventIT/FrienshipSistem/tests.py
+++ EventIT/FrienshipSistem/tests.py
@@ -1,6 +1,6 @@
 import unittest
 from EventIT.UsersLib.CitizenClass import Ciudadano
-from EventIT.FriendshipSistem.FrienshiSist import Frienship_Sistem
+from EventIT.FriendshipSistem.FrienshiSist import Frienship_System
 from EventIT.UsersLib.RegDeUsuarios import RegDeUsuarios
 
 
@@ -13,7 +13,7 @@ class MyTestCase(unittest.TestCase):
         RegUsr.Manage_Ciudadanos(Juan,True,'JUAN')
         RegUsr.Manage_Ciudadanos(tom,True,"tom")
         self.assertEqual(RegUsr.Get_Ciudadanos(),{'JUAN': [Juan, 0], 'tom': [tom, 0]})
-        Friendship = Frienship_Sistem(RegUsr)
+        Friendship = Frienship_System(RegUsr)
         Friendship.EnviarSolicitud(123,999,111,000)
         self.assertEqual(tom.Get_ListaDeSolicitudes(),[Juan])
 
@@ -26,7 +26,7 @@ class MyTestCase(unittest.TestCase):
         RegUsr.Manage_Ciudadanos(Marco,True,"Marco")
         RegUsr.Manage_Ciudadanos(Luis,True,"Luis")
         RegUsr.Manage_Ciudadanos(Julio,True,"Julio")
-        sist = Frienship_Sistem(RegUsr)
+        sist = Frienship_System(RegUsr)
         sist.EnviarSolicitud(987,111,100,300)
         sist.EnviarSolicitud(987,000,100,200)
         sist.AceptarSolicitud(987,111,100,300)
