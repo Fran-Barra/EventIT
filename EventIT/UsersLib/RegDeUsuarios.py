be9bd713f1fd31a675749bb2e5fb81be4cfb31cd
luckylovaglio <luckylovaglio@gmail.com>
2021-11-17 14:46:37 -0300

    update Estadisticas.py and RegDeUsuarios.py

--- EventIT/UsersLib/RegDeUsuarios.py
+++ EventIT/UsersLib/RegDeUsuarios.py
@@ -7,6 +7,9 @@ class RegDeUsuarios:
     def __init__(self):
         self.__Admins = dict({})
         self.__Ciudadanos = dict({})
+        with open(r'C:\Users\lucky\PycharmProjects\EventIT\EventIT\UsersLib\registro_de_usuarios.txt','w') as f:
+            f.write(f'Lista de usuarios registrados: \n')
+            f.close()
 
     def Get_Admins(self):
         return self.__Admins.copy()
@@ -21,11 +24,27 @@ class RegDeUsuarios:
         """Permite agregar o eliminar un admin del dicccionario de administradores.\n
             add = True, para agregarlo.\n
             add = False, para eliminarlo"""
+        user_line = f'Keyname: {keyname}/Name: {admin.Get_Name()}\n'
+        path = r'C:\Users\lucky\PycharmProjects\EventIT\EventIT\UsersLib\registro_de_usuarios.txt'
         if add:
             self.__Admins[keyname] = admin
+            with open(path,'a') as f: # agregar texto sin sobreescribir
+                f.write(user_line)
+                f.close()
         else:
             try:
                 del self.__Admins[keyname]
+                with open(path,'r') as f: # codigo para borrar usuarios
+                    lineas = f.readlines()
+                    with open(path,'w') as f:
+                        f.write('')
+                        f.close()
+                    for linea in lineas:
+                        if linea != user_line:
+                            with open(path,'a') as f:
+                                f.write(linea)
+                                f.close()
+                    f.close()
             except KeyError:
                 alert = tk.messagebox.showwarning(title="Error en el Keyname", text="El keyname que ingreso no pertenece a ningún administrador.")
 
@@ -36,10 +55,30 @@ class RegDeUsuarios:
         """Permite agregar o eliminar un ciudadano del dicccionario de ciudadanos.\n
             add = True, para agregarlo.\n
             add = False, para eliminarlo"""
+        user_line = (f'Keyname: {keyname}/Name: {ciudadano.Get_Name()}/Telefono Celular: {ciudadano.Get_Telefono()}/'
+                        f'Cuil: {ciudadano.Get_Cuil()}/Contactos: {ciudadano.Get_ContactosDeInteres()}/'
+                        f'Lista de solicitudes: {ciudadano.Get_ListaDeSolicitudes()}/Lista de rechazos: {ciudadano.Get_ListaDeRechazos()}\n')
+        path = r'C:\Users\lucky\PycharmProjects\EventIT\EventIT\UsersLib\registro_de_usuarios.txt'
         if add:
             self.__Ciudadanos[keyname] = [ciudadano, 0]
+            with open(path,'a') as f: # agregar texto sin sobreescribir
+                f.write(user_line)
+                f.close()
         else:
             del self.__Ciudadanos[keyname]
+            with open(path,'r') as f: # codigo para borrar usuarios
+                lineas = f.readlines()
+                with open(path,'w') as f:
+                    f.write('')
+                    f.close()
+                for linea in lineas:
+                    if linea != user_line:
+                        with open(path,'a') as f:
+                            f.write(linea)
+                            f.close()
+                f.close()
+
+
 
     def estado_de_bloqueo(self, bloquear: bool, keyname):
         self.__Ciudadanos[keyname][1] = bloquear
