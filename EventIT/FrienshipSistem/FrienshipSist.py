80eb557f4166fc8f2257c9a6dff9b8c00e26b0d0
Your Name <you@example.com>
2021-11-18 20:21:01 -0300

    Comprobando friendship y registro txt

--- EventIT/FrienshipSistem/FrienshipSist.py
+++ EventIT/FrienshipSistem/FrienshipSist.py
@@ -11,7 +11,7 @@ class Frienship_System:
             CiudadanoDestinatario = regdeusuarios.searchCitizen(CelDestinatario,CuilDestinatario,NameDestinatario)
             if len(CiudadanoSolicitante.Get_ListaDeRechazos()) < 5:
                 if CiudadanoSolicitante not in CiudadanoDestinatario.Get_ListaDeSolicitudes():
-                    CiudadanoDestinatario.Mod_ListaDeSolicitudes().append(CiudadanoSolicitante)
+                    CiudadanoDestinatario.Mod_ListaDeSolicitudes(CiudadanoSolicitante, True)
                 else:
                     messagebox.showwarning(title= "Already sent", message= "You already sent a request to this user")
             else:
@@ -30,9 +30,9 @@ class Frienship_System:
             CiudadanoDestinatario = regdeusuarios.searchCitizen(CelDestinatario,CuilDestinatario,NameDestinatario)
             if len(CiudadanoSolicitante.Get_ListaDeRechazos()) < 5:
                 if CiudadanoSolicitante in CiudadanoDestinatario.Get_ListaDeSolicitudes():
-                    CiudadanoDestinatario.Mod_ContactosDeInteres().append(CiudadanoSolicitante)
-                    CiudadanoDestinatario.Mod_ListaDeSolicitudes().remove(CiudadanoSolicitante)
-                    CiudadanoSolicitante.Mod_ContactosDeInteres().append(CiudadanoDestinatario)
+                    CiudadanoDestinatario.Mod_ContactosDeInteres(CiudadanoSolicitante,True)
+                    CiudadanoDestinatario.Mod_ListaDeSolicitudes(CiudadanoSolicitante, False)
+                    CiudadanoSolicitante.Mod_ContactosDeInteres(CiudadanoDestinatario, True)
                 else:
                     messagebox.showwarning(title= "No requests", message= "You do not have requests from this user")
             else:
@@ -50,8 +50,8 @@ class Frienship_System:
             CiudadanoSolicitante = regdeusuarios.searchCitizen(CelSolicitante,CuilSolicitante,NameSolicitante)
             CiudadanoDestinatario = regdeusuarios.searchCitizen(CelDestinatario,CuilDestinatario,NameDestinatario)
             if CiudadanoSolicitante in CiudadanoDestinatario.Get_ListaDeSolicitudes():
-                CiudadanoDestinatario.Mod_ListaDeSolicitudes().remove(CiudadanoSolicitante)
-                CiudadanoSolicitante.Mod_ListaDeRechazos().append(CiudadanoDestinatario)
+                CiudadanoDestinatario.Mod_ListaDeSolicitudes(CiudadanoSolicitante, True)
+                CiudadanoSolicitante.Mod_ListaDeRechazos(CiudadanoDestinatario, True)
 
             else:
                 messagebox.showwarning(title= "No requests", message= "You do not have requests from this user")
